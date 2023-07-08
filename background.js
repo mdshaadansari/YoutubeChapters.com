33a7eb9501ffa15302eb5fd91ccb5102a3ffb8d4
mdshaadansari <113072356+mdshaadansari@users.noreply.github.com>
2023-07-08 14:38:49 +0530

    Create background.js

---
+++ background.js
@@ -0,0 +1,26 @@
+// Background script (background.js)
+
+// Function to send a message to the content script
+function sendMessageToContentScript(message) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+      } else {
+        console.log('Shortened Line:', response.shortenedLine);
+      }
+    });
+  });
+}
+
+// Function to handle the click event on the browser action icon
+function handleBrowserActionClick() {
+  const line = prompt('Enter a line to shorten:');
+  if (line) {
+    const message = { action: 'shortenLine', line: line };
+    sendMessageToContentScript(message);
+  }
+}
+
+// Add a listener for the browser action click event
+chrome.browserAction.onClicked.addListener(handleBrowserActionClick);
