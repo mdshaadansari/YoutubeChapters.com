4e317440b44fc6d71b40831d323a2713d2f71a47
mdshaadansari <113072356+mdshaadansari@users.noreply.github.com>
2023-07-09 12:46:25 +0530

    background.js

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 // Background script (background.js)
 
-// Function to send a message to the content script
+// Function to send a message to the content script....
 function sendMessageToContentScript(message) {
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
